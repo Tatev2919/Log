8e99a5f14d9dad4a7f6d1775307f1ba0c38a7415
Tatev Karapetyan <tatev@karapetian.ru>
2021-06-20 22:45:03 +0400

    aa

--- log_parse.tcl
+++ log_parse.tcl
@@ -2,7 +2,6 @@
 
 proc read_in_file { in_file } {
 	set datas [dict create] 
-	#set list1 = [list "Golden elab runtime"  "Revised elab runtime" "Golden compile rt" "Revised compile rt" "Set mode ec rt" "Map rt" "Compare rt" "Mapped inputs" "Mapped outputs " "Mapped states"]
 	set pf [open $in_file r]
 	puts "the file $in_file will be parsed"
 	parse_file $pf $datas
@@ -11,32 +10,41 @@ proc read_in_file { in_file } {
 
 proc parse_file { f_name ,datas } {
 	set mf [open "Mapping.tmp" w]
+	set list1 [list "Golden elab runtime"  "Revised elab runtime" "Golden compile rt" "Revised compile rt" "Set mode ec rt" "Map rt" "Compare rt" "Mapped inputs" "Mapped outputs " "Mapped states" ]
+    set var ""
+    foreach i $list1 {
+        append var "\"" "$i" "\"" ","   
+    }
+    puts $mf $var
 	set s 0
 	set n 0
 	set m 0
+    set flag 0
 	set key ""
 	while {[gets $f_name line] != -1} {
 		if { $n == 1 && [string match "*$s*" $line] == 1} {
 			set s 0
 			set n 0
-			dict append datas $key $line
+			dict append datas $key [string range $line 13 21]
+            set key1 [ string last "_" $key ]
+            set key11 [ string range $key 0 $key1]
+            append key11 "Mem"
+			dict append datas $key11 [string range $line 38 48]
 		}
 		if { $m == 1 } {
 			if { [string match "*$s*" $line] == 1} {
-				#puts $line 
 			    dict append datas $key $line
 				set s 0
 				set m 0
-			#	set key ""
 			} else {
-				puts $mf $line
+				#puts $mf $line
 			    dict append datas $key $line 
 			}
 		}
 		if { [string match "*Elaboration of design golden*" $line] == 1} {
 			set s "System"
 			set n 1
-			set key "El_golden_time" 
+			set key "El_golden_time"
 		}
 		if { [string match "*Elaboration of design revised*" $line] == 1} {
 			set s "System"
@@ -45,11 +53,21 @@ proc parse_file { f_name ,datas } {
 		}
 		if { [string match "*Compilation for design golden*" $line] == 1} {
 			set key "Comp_golden_time"
-			dict append datas $key $line
+            puts $key
+            set x [ string first took $line ]
+            set y [ expr $x + 5 ]
+            set space [ string first " " $line $y ]
+            set sec [ expr $space + 3 ]
+			dict append datas $key [ string range $line $y $sec ]
 		}
 		if { [string match "*Compilation for design revised*" $line] == 1} {
 			set key "Comp_revised_time"
-			dict append datas $key $line
+            puts $line
+            set x [ string first took $line ]
+            set y [ expr $x + 5 ]
+            set space [ string first " " $line $y ]
+            set sec [ expr $space + 3 ]
+			dict append datas $key [ string range $line $y $sec ]
 		}
 		if { [string match "*Entering ec mode*" $line] == 1} {
 			set s "*System*"
@@ -62,46 +80,27 @@ proc parse_file { f_name ,datas } {
 			set key "Mapping_info"
         }
 		if { [string match "*The designs are*" $line] == 1} {
-			set key "Eq?Not"
+            set key "Eq?Not"
 			set n 1
-			dict append datas $key $line
+            set x [ string first are $line ]
+            set y [ expr $x + 3 ]
+            set eq [ string first equiv $line $y ]
+            set eqI [ expr $eq + 4]
+            if { $flag == 0 } {
+			    dict append datas $key [string range $line $y $eqI]
+            }
+            set flag  1
 			set key "Design_Time"
 			set s "*System*"
 			set n 1
 		}
-		if { [string match "*resource_usage_after_ec_mode_setup*" $line ] == 1 } {
-			set key "Res_after_ec_mode"
-			set s "*System*"
-			set n 1
-		}
-		if { [string match "*resource_usage_after_map*" $line ] == 1 } {
-            set key "Res_after_map"
-            set s "*System*"
-            set n 1
-        }
-		if { [string match "*resource_usage_after_compare*" $line ] == 1 } {
-            set key "Res_after_compare"
-            set s "*System*"
-            set n 1
-        }
-		if { [string match "*resource_usage_after_clearbox*" $line ] == 1 } {
-            set key "Res_after_clearbox"
-            set s "*System*"
-            set n 1
-        }
-		if { [string match "*resource_usage_after_golden*" $line ] == 1 } {
-            set key "Res_after_golden_elab"
-            set s "*System*"
-            set n 1
-        }
-		if { [string match "*resource_usage_after_revised*" $line ] == 1 } {
-            set key "Res_after_revised_elab"
-            set s "*System*"
-            set n 1
-        }
 		if { [string match "*Check * finished*" $line] == 1} {
 			set key "comb?seq"
-			dict append datas $key $line
+            set x [ string first ( $line ]
+            set y [ expr $x + 1 ]
+            set bracket [ string first ) $line $y ]
+            set brack [ expr $bracket ] 
+			dict append datas $key [ string range $line $y $brack ]
 		}
 
 	}
@@ -109,12 +108,12 @@ proc parse_file { f_name ,datas } {
 	puts  $datas
 	close $f_name
 	close $mf
-	set mf [open "Mapping.tmp" r]
-	while {[gets $mf l] != -1} {
-		puts $l
-	}
-	close $mf
-	file delete "Mapping.tmp"
+	#set mf [open "Mapping.tmp" r]
+	#while {[gets $mf l] != -1} {
+	#	puts $l
+	#}
+	#close $mf
+	#file delete "Mapping.tmp"
 }
 
 if { $argc > 0   } {
